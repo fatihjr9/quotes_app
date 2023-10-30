e7eeab4f36dcf9cab40ae6aaa96ce80160b0ad09
fatihjr9 <malikfatihibrahim@gmail.com>
2023-10-31 00:02:47 +0700

    update client UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,20 @@ function App() {
   },[])
   return (
     <Layout>
-      <input type="text" placeholder="dddd" className="ring-1" />
-      {
-        showQuote.map((item) => (
-          <div key={item.id}>
-            <h5>{item.author}</h5>
-          </div>
-        ))
-      }
+      <div className="mx-auto text-center -translate-y-12">
+        <input type="text" placeholder="dddd" className="ring-1" />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+        {
+          showQuote.map((item) => (
+            <div key={item.id} className="ring-1 ring-gray-500 p-2 rounded-md">
+              <h5>{item.quote}</h5>
+              <h5>{item.author}</h5>
+            </div>
+          ))
+        }
+      </div>
     </Layout>
   );
 }
